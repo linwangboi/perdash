87275f5ef4920e047793cae5817255889c01666a
Sadie <an5dy90@gmail.com>
2026-04-11 00:25:41 +0800

    feat: logo-full-red

--- frontend/app/(auth)/layout.js
+++ frontend/app/(auth)/layout.js
@@ -6,13 +6,15 @@ const Layout = ({ children }) => {
     <div className="flex min-h-screen">
       <section className="bg-brand p-10 items-center justify-center hidden w-1/2 lg:flex xl:w-2/5">
         <div className="flex max-h-200 max-w-107.5 flex-col justify-center space-y-12">
-          <Image
-            src="/assets/logo-full.svg"
-            alt="logo"
-            width={224}
-            height={82}
-            className="h-auto invert"
-          />
+          <div className="flex justify-center">
+            <Image
+              src="/assets/logo-full.svg"
+              alt="logo"
+              width={224}
+              height={82}
+              className="h-auto invert"
+            />
+          </div>
           <div className="space-y-5 text-white">
             <h1 className="h1">Jog down everything!</h1>
             <p className="body-1">
@@ -31,7 +33,7 @@ const Layout = ({ children }) => {
       <section className="flex flex-col flex-1 items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
         <div className="mb-16 lg:hidden">
           <Image
-            src="/assets/logo-full.svg"
+            src="/assets/logo-full-red.svg"
             alt="logo"
             width={224}
             height={82}
