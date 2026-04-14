0f6754dda78889c0bd7613fa197c98837ef35efe
Sadie <an5dy90@gmail.com>
2026-04-14 17:28:30 +0800

    feat:sidebar

--- frontend/components/Sidebar.jsx
+++ frontend/components/Sidebar.jsx
@@ -1,22 +1,67 @@
-'use client'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import { navItems } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
-const Sidebar = ({firstName, email}) => {
+const Sidebar = ({ firstName, email }) => {
+  const pathname = usePathname();
   return (
-    <aside className='sidebar'>
-        <Link href='/'>
-            <Image src='/assets/logo-full.svg' alt='logo' width={160} height={50} className='hidden h-auto lg:block'/>
-            <Image src='/assets/logo-full-red.svg' alt='logo' width={52} height={55} className='lg:hidden'/>
-        </Link>
-        <nav className='sidebar-nav'>
-            <ul className='flex flex-col gap-6 flex-1'>
-                
-            </ul>
-        </nav>
+    <aside className="sidebar">
+      <Link href="/">
+        <Image
+          src="/assets/logo-full.svg"
+          alt="logo"
+          width={160}
+          height={50}
+          className="hidden h-auto ml-6 lg:block"
+        />
+        <Image
+          src="/assets/logo-full-red.svg"
+          alt="logo"
+          width={52} 
+          height={55}
+          className="lg:hidden"
+        />
+      </Link>
+      <nav className="sidebar-nav">
+        <ul className="flex flex-col gap-6 flex-1">
+          {navItems.map(({ url, name, icon }) => (
+            <Link key={name} href={url} className="lg:w-full">
+              <li
+                className={cn(
+                  "sidebar-nav-item",
+                  pathname === url && "shad-active",
+                )}
+              >
+                <Image
+                  src={icon}
+                  alt={name}
+                  width={24}
+                  height={24}
+                  className={cn(
+                    "nav-icon",
+                    pathname === url && "nav-icon-active",
+                  )}
+                />
+                <p className="hidden lg:block">{name}</p>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </nav>
+      <Image src='/assets/files.png' alt='logo' width={506} height={418} className="w-full p-2" />
+      <div className='sidebar-user-info'>
+        <Image src='/assets/avatar.png' alt='avatar' width={44} height={44} className="sidebar-user-avatar" />
+        <div className="hidden lg:block">
+          <p className="subtitle-2 capitalize">first last</p>
+          <p className="caption">email@example.com</p>
+        </div>
+      </div>
     </aside>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
---
+++ frontend/constants/index.js
@@ -0,0 +1,27 @@
+export const navItems = [
+  {
+    name: "Today",
+    icon: "/assets/today.svg",
+    url: "/",
+  },
+  {
+    name: "All Tasks",
+    icon: "/assets/all.svg",
+    url: "/all",
+  },
+  {
+    name: "Upcoming",
+    icon: "/assets/upcoming.svg",
+    url: "/upcoming",
+  },
+  {
+    name: "Important",
+    icon: "/assets/important.svg",
+    url: "/important",
+  },
+  {
+    name: "Completed",
+    icon: "/assets/completed.svg",
+    url: "/completed",
+  },
+];
