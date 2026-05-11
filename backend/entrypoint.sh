258b1f1683adbdbd6be1665c502c3c19803f9e79
linwangboi <linwangboi@gmail.com>
2026-05-11 08:39:28 +0000

    fix: celery worker concurrency too high, set to 0, remove root prvlg for celery worker

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -18,7 +18,7 @@ python manage.py migrate
 # Decide which process to start
 if [ "$RAILWAY_PROCESS_TYPE" = "worker" ]; then
     echo "Starting Celery worker..."
-    celery -A perdash worker --loglevel=info
+    celery -A perdash worker --loglevel=info --concurrency=1 --uid=1000
 else
     echo "Starting Gunicorn web server..."
     exec gunicorn perdash.wsgi:application --bind 0.0.0.0:10000
